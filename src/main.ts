83b062a5a8556c35eb86952e811889ebdb2fb2e6
Ali Waseem <waseema393@gmail.com>
2021-04-18 11:34:41 -0600

    Feature: Add REST-API layer (#4)
    * add routes for status and checking IP
    
    * added routes for server and check IP
    
    * added unit tests for handler

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import config from './config/fireholLists.json'
 import { createBlockListIP } from './models/blockLists'
 import { createFetch } from './models/fetch'
 import { createTrie } from './models/trie'
 import { createTrieUpdater } from './process/trieUpdater'
-import config from './config/fireholLists.json'
+import { createServer, registerPlugins } from './server/server'
+import { addStatusRoute } from './routes/status'
+import { addIpCheckRoute } from './routes/checkIP'
+
+const DEFAULT_PORT = 8080
+const DEFAULT_ADDRESS = '0.0.0.0'
 
 async function main(): Promise<void> {
   // initialize models
@@ -13,11 +19,30 @@ async function main(): Promise<void> {
   // initialize processes
   const trieUpdater = createTrieUpdater(blockListIP, trie)
 
+  // Build server
+  const server = createServer()
+
+  // Apply middleware
+  await registerPlugins(server)
+
+  // Add routes
+  addStatusRoute(server)
+  addIpCheckRoute(server, trie)
+
   // Update Trie
   await trieUpdater.update(config)
 
   // Starting polling for changes
   trieUpdater.pollingUpdate(config)
+
+  // Start the server
+  const port = process.env.ICUIP_PORT
+    ? Number(process.env.ICUIP_PORT)
+    : DEFAULT_PORT
+  const address = process.env.ICUIP_ADDRESS ?? DEFAULT_ADDRESS
+
+  const url = await server.listen(port, address)
+  console.log(`Server listening at ${url}`)
 }
 
 main()
---
+++ src/routes/checkIp.ts
@@ -0,0 +1,78 @@
+import { FastifyReply } from 'fastify'
+import { Trie } from '../models/trie'
+import { FastifyServer, RequestGeneric } from '../server/server'
+
+const VERSION = 'v1'
+const CHECK_IP_ROUTE = 'check/:ip'
+
+export interface CheckIpRequest {
+  readonly Params: {
+    readonly ip: string
+  }
+}
+
+export function addIpCheckRoute(server: FastifyServer, trie: Trie): void {
+  const checkIpRouteOptions = {
+    schema: {
+      description: 'Check if IP is valid or not',
+      summary: 'Check IP',
+      tags: ['check'],
+      params: {
+        title: 'IP address parameter',
+        type: 'object',
+        properties: {
+          ip: {
+            type: 'string',
+            format: 'ipv4',
+            description: 'IPv4 address to check weather its valid or not',
+          },
+        },
+        additionalProperties: false,
+        required: ['ip'],
+      },
+      response: {
+        200: {
+          title: 'IP Address check response',
+          type: 'object',
+          properties: {
+            valid: {
+              type: 'boolean',
+              description: 'weather the value is correct or not',
+            },
+            source: {
+              type: 'string',
+              description: 'file name that has the matching ip cidr or address',
+            },
+          },
+          additionalProperties: false,
+          required: ['valid'],
+        },
+      },
+    },
+  }
+
+  function checkIpHandler(
+    req: RequestGeneric<CheckIpRequest>,
+    reply: FastifyReply,
+  ): void {
+    const { ip } = req.params
+    const check = trie.search(ip)
+
+    const response = check
+      ? {
+          valid: false,
+          source: check,
+        }
+      : {
+          valid: true,
+        }
+
+    reply.send(response)
+  }
+
+  server.get(
+    `/${VERSION}/${CHECK_IP_ROUTE}`,
+    checkIpRouteOptions,
+    checkIpHandler,
+  )
+}
