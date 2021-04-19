91bcb04d42c76ab8b9dafb15a2fdac69b7a71009
Ali Waseem <github@aliwaseem.com>
2021-04-19 09:07:31 -0600

    updated load test script

--- k6/randomIpChecks.js
+++ k6/randomIpChecks.js
@@ -9,7 +9,7 @@ export let options = {
   scenarios: {
     constant_request_rate: {
       executor: 'constant-arrival-rate',
-      rate: 2500,
+      rate: 1000, // Modified this from 1000, 2000, 4000
       timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
       duration: '30s',
       preAllocatedVUs: 1000, // how large the initial pool of VUs would be
