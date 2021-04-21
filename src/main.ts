54e1547321e8d5514985f66c129b347af700073e
Ali Waseem <github@aliwaseem.com>
2021-04-20 19:02:19 -0600

    grammer fix

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import { createServer } from './server/server'
 import { createModels } from './models/models'
 
 async function main(): Promise<void> {
-  // initialize models
+  // Initialize models
   const { blockLists, trie } = createModels()
 
-  // initialize trie updater and fill trie with IPs
+  // Initialize trie updater and fill trie with IPs
   const trieUpdater = createTrieUpdater(blockLists, trie)
   await trieUpdater.update(config)
 
-  // initialize server
+  // Initialize server
   const server = createServer(trie)
   await server.start()
 
